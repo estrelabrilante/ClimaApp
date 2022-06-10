722db08b935cea9b3757d023708454fc9f5de8e6
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-06-10 10:40:10 -0400

    updated Text Field
    Protocol UITextField delegate

--- Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -3,23 +3,62 @@
 //  Clima
 //
 //  Created by Angela Yu on 01/09/2019.
-// learned by Shynu Mary Varghese
 //  Copyright © 2019 App Brewery. All rights reserved.
 //
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
+    @IBOutlet weak var searchTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       //textfield will report to self which is weather viewController
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+       
+        print(searchTextField.text!)
+        
+        //dismiss keyboard
+        searchTextField.endEditing(true)
+        
+    }
+    //when the user click GO key in soft keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       
+        print(searchTextField.text!)
+        //dismiss keyboard
+        searchTextField.endEditing(true)
+        return true
+    }
+  //clear textField after end editing is true.
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //use searchTextField.text to get the weather for that city.
+        //resetting
+        searchTextField.text = ""
+    }
+    //validation on user typed
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        //any textField could triggered
+        if textField.text != ""{
+            return true
+        }
+        else{
+            textField.placeholder = "Type something here"
+            return false
+        }
+        
+    
+    
+    
 }
 
+}
